8ba3222f0e62050a292e9eceec9c1a822ccfa5e5
harriprst <harryprasath.venkatachalam@gmail.com>
2024-12-31 23:56:55 +0530

    initial commit

---
+++ src/main/java/com/src/main/currencyconversionserver/CurrencyConversionServerApplication.java
@@ -0,0 +1,13 @@
+package com.src.main.currencyconversionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConversionServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/src/main/currencyconversionserver/CurrencyConversionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.main.currencyconversionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
